91c308ce9fc28feb9ef2457aeda095dd40d007c0
Radovan <60538859+wildfoxcz@users.noreply.github.com>
2021-09-02 16:36:57 +0200

    fixed redirect
    fixed redirect

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = './landing';
+    protected $redirectTo = '../landing';
 
     /**
      * Create a new controller instance.
